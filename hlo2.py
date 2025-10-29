efb4d727ae010b4afdb08bd583404ebc3ce500da
veereshn696-alt <veereshn696@gmail.com>
2025-10-29 23:15:01 +0530

    origin

---
+++ hlo2.py
@@ -0,0 +1,9 @@
+marks=int(input("enter marks :"))
+if(marks>90) :
+    print("grade A")
+elif(marks>80) :
+    print("grade B")
+elif(marks>70) :
+    print("grade C")
+elif(marks>60) :
+    print("grade D")
